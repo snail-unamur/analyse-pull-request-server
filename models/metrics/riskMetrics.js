29519525b2817c47dde9e4e9a96b7ac46cb14a6e
hraskin <raskin.hugo@gmail.com>
2025-10-14 19:00:00 +0200

    Area for the formula

--- models/metrics/riskMetrics.js
+++ models/metrics/riskMetrics.js
@@ -6,22 +6,17 @@ const calculateRiskMetric = (defaultMetrics, radarMetrics) => {
     const coordofAxes = polarToCartesian(radarMetrics);
     const area = shoelaceCalculation(coordofAxes);
 
-    const fileFactor = defaultMetrics.find(m => m.id === 'files')?.value || 1;
-    const lineFactor = defaultMetrics.find(m => m.id === 'ncloc')?.value || 1;
-    const coverage = defaultMetrics.find(m => m.id === 'new_coverage')?.value || 1;
-
-    const rawValue = area / ((1 / fileFactor) * (1 / lineFactor) * coverage);
-    return Math.round(rawValue);
+    return area.toFixed(2);
 }
 
 const polarToCartesian = (axes) => {
     const angleStep = (2 * Math.PI) / axes.length;
-    const origin = Math.PI / 2; // Point at the top of the chart
+    const origin = Math.PI / 2;
 
     return axes.map((a, i) => {
         const thetaI = origin - (i * angleStep);
-        const x = a.value * Math.cos(thetaI);
-        const y = a.value * Math.sin(thetaI);
+        const x = a.radarValue * Math.cos(thetaI);
+        const y = a.radarValue * Math.sin(thetaI);
         return { x, y };
     });
 };
